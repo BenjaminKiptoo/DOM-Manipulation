a0d02da14ef33132d678a2253695e339d0c38885
Benjamin <kiptoo925@gmail.com>
2022-06-01 19:11:03 +0300

    DOM manipulation to add or remove text on webpage

--- scripts.js
+++ scripts.js
@@ -1,13 +1,37 @@
 $("document").ready(function(){
   $("button#hello").click(function(){
-    $("ul").prepend("<li>HELLO!</li>");
+    $("ul#user").prepend("<li>HELLO!</li>");
+    $("ul#webpage").prepend("<li>WHY HELLO THERE</li>");
+    $('li').css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function(){
-    $("ul").prepend("<li>GOODBYE!</li>");
+    $("ul#user").after("<li>GOODBYE!</li>");
+    $("ul#webpage").after("<li>GOOODBYE,SEE YOU SOON</li>");
+    $("li").css("background-color" , "red");
+    $("ul#user").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function(){
-    $("ul").prepend("<li>STOP COPYING ME</li>")
-  })
+    $("ul#user").append("<li>STOP COPYING ME</li>");
+    $("ul#webpage").append("<li>I JUST WANT A FRIEND</li>")
+  });
+  $("li").css("background-color" , "yellow");
+    $("ul#user").children("li").first().click(function(){
+      $(this).remove();
+    })
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    })
 });
